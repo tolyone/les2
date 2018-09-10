59e9ef01234bf414f3d0ce37285c0b1c508b7186
b16-601 <b16-601@csilab.local>
2018-09-10 17:51:57 +0300

    Initial commit

---
+++ les2.py
@@ -0,0 +1,22 @@
+import math as mat
+
+print('input number a=', end='')
+a = int(input())
+
+print('Input nubmer b=', end='')
+b = int(input())
+
+print (a,b)
+
+def foo(a,b):
+    print('a+b=',a+b)
+    print('a-b=',a-b)
+    print('a/b=',a/b)
+    print('a*b=',a*b)
+    print('a**b=',a**b)
+    print('sqrt(a)=',mat.sqrt(a))
+    print('sqrt(b)=',mat.sqrt(b))
+
+foo(a,b)
+
+# принимает любое число, внутри функции корень, умножение, деление
